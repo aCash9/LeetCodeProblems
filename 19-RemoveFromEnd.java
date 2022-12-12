40bc4451d1898acd95be7365f8a75f64c2a6e71d
aCash9 <113261623+aCash9@users.noreply.github.com>
2022-12-12 23:35:20 +0530

    Create 19-RemoveFromEnd.java

---
+++ 19-RemoveFromEnd.java
@@ -0,0 +1,15 @@
+public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        for (int i = 0; i <= n && fast!=null ; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow=slow.next;
+            fast=fast.next;
+        }
+        slow.next=slow.next.next;
+        return dummy.next;
+    }
