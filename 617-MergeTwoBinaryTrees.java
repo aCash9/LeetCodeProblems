d59a67f8b6be6f487f68cb9a435d5cc19ad9b657
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2023-02-10 12:28:11 +0530

    better solution added below

--- 617-MergeTwoBinaryTrees.java
+++ 617-MergeTwoBinaryTrees.java
@@ -36,6 +36,22 @@ class Solution {
         }
 
         return merged;
-
     }
+    // better solution
+    class Solution {
+    public TreeNode mergeTrees(TreeNode r1, TreeNode r2) {
+        if(r1 == null)
+            return r2;
+            
+        if(r2 == null)
+            return r1;
+
+        r1.val += r2.val;
+
+        r1.left = mergeTrees(r1.left,r2.left);
+        r1.right = mergeTrees(r1.right,r2.right);
+        
+        return r1;
+    }
+}
 }
