8cfc37a9e8184d0bd9df411c07a4a81a33efa99e
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2023-02-19 22:05:22 +0530

    Create 118-Pascal'sTriangle.java

---
+++ 118-Pascal'sTriangle.java
@@ -0,0 +1,10 @@
+class Solution {
+    public List<Integer> row(List<Integer> l)
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new LinkedList<>();
+        for(int i = 0; i < numRows; i++){
+            List<Integer> l = new LinkedList<>();
+            
+        }
+    }
+}
