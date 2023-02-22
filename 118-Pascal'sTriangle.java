443bc337128376546713b44e04d2875a5d7daf86
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2023-02-22 13:54:17 +0530

    Update 118-Pascal'sTriangle.java

--- 118-Pascal'sTriangle.java
+++ 118-Pascal'sTriangle.java
@@ -1,10 +1,23 @@
 class Solution {
-    public List<Integer> row(List<Integer> l)
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list = new LinkedList<>();
-        for(int i = 0; i < numRows; i++){
-            List<Integer> l = new LinkedList<>();
-            
+        List<List<Integer>> triangle = new LinkedList<>();
+        if(numRows == 0)
+            return triangle;
+        List<Integer> first_row = new LinkedList<>();
+        first_row.add(1);
+        triangle.add(first_row);
+
+        for(int i = 1; i < numRows; i++){
+           List<Integer> prev = triangle.get(i-1);
+           List<Integer> curr = new LinkedList<>();
+
+           curr.add(1);
+           for(int j = 1; j < i; j++)
+               curr.add(prev.get(j - 1) + prev.get(j));
+           curr.add(1);
+
+            triangle.add(curr);
         }
+        return triangle;
     }
 }
