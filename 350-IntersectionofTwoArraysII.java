abf762fbc969e42b715f651d40cdc8bfa4709e6c
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2023-02-25 19:54:31 +0530

    A better solution added below

--- 350-IntersectionofTwoArraysII.java
+++ 350-IntersectionofTwoArraysII.java
@@ -24,3 +24,23 @@ class Solution {
         return array;
     }
 }
+// better solution
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+       Arrays.sort(nums1);
+       Arrays.sort(nums2);
+       int[] ans = new int[nums1.length + nums2.length];
+       int i = 0 , j = 0, k = 0;
+       while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j])
+                i++;
+            else if(nums1[i] > nums2[j])
+                j++;
+            else {
+                ans[k++] = nums1[i++];
+                j++;
+            } 
+       }
+       return Arrays.copyOfRange(ans, 0, k);
+    }
+}
