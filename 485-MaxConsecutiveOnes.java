b29cfd6770345782bd7973c229e6bc3ce59bdc21
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2023-02-27 16:32:37 +0530

    better solution added

--- 485-MaxConsecutiveOnes.java
+++ 485-MaxConsecutiveOnes.java
@@ -13,3 +13,20 @@ class Solution {
         return maxFreq;
     }
 }
+// better solution
+public int[] findErrorNums(int[] nums) {
+        int[] index = new int[nums.length + 1];
+        Arrays.fill(index, 0);
+        int[] arr = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            index[nums[i]]++; 
+            if(index[nums[i]] > 1)
+                arr[0] = nums[i];
+        } 
+        for(int i = 1; i < index.length; i++)
+            if(index[i] == 0){
+                arr[1] = i;
+                break;
+            }
+        return arr;
+    }
