93651b92a7ca53bf81353bad44aaa1f282b9d945
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2023-02-28 16:10:12 +0530

    Create 1037-ValidBoomerang.java

---
+++ 1037-ValidBoomerang.java
@@ -0,0 +1,5 @@
+class Solution {
+    public static boolean isBoomerang(int[][] p) {
+       return (p[0][0] * (p[1][1] - p[2][1]) + p[1][0] * (p[2][1] - p[0][1]) + p[2][0] * (p[0][1] - p[1][1])) != 0;
+    }
+}
