dc2f8c5968738be6cde0d1d5739709c83e0552de
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2023-04-08 16:08:09 +0530

    Update 6-ZigzagConversion.java

--- 6-ZigzagConversion.java
+++ 6-ZigzagConversion.java
@@ -34,3 +34,37 @@ class Solution {
         return ans;
     }
 }
+
+//other approach
+public static String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+        int row = 0;
+        boolean goingDown = true;
+        for (char ch : s.toCharArray()) {
+            rows[row].append(ch);
+            if (goingDown) {
+                row++;
+                if (row == numRows) {
+                    row = numRows - 2;
+                    goingDown = false;
+                }
+            } else {
+                row--;
+                if (row == -1) {
+                    row = 1;
+                    goingDown = true;
+                }
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        for (StringBuilder r : rows) {
+            ans.append(r);
+        }
+        return ans.toString();
+    }
