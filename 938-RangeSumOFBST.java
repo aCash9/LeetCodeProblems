fad3885812df3c9ff7097a60f3fc9fceab903432
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2023-12-15 00:16:52 +0530

    Update 938-RangeSumOFBST.java

--- 938-RangeSumOFBST.java
+++ 938-RangeSumOFBST.java
@@ -17,6 +17,13 @@ class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root == null)    return 0;
 
+        if(root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+        if(root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+
         int val = root.val, sum = 0;
         if(val >= low && val <= high)
             sum += rangeSumBST(root.left, low, high) +
@@ -24,7 +31,7 @@ class Solution {
         else 
             sum += rangeSumBST(root.left, low, high) +
                     rangeSumBST(root.right, low, high);
-        
+
         return sum;
     }
 }
