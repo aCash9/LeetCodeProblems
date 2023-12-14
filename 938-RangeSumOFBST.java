4a48f8738d1f977cc0de4fb87c80878e96aaae9e
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2023-12-14 23:53:17 +0530

    Create 938-RangeSumOFBST.java

---
+++ 938-RangeSumOFBST.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null)    return 0;
+
+        int val = root.val, sum = 0;
+        if(val >= low && val <= high)
+            sum += rangeSumBST(root.left, low, high) +
+                    rangeSumBST(root.right, low, high) + val;
+        else 
+            sum += rangeSumBST(root.left, low, high) +
+                    rangeSumBST(root.right, low, high);
+        
+        return sum;
+    }
+}
