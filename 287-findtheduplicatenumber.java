9a8b6d7bebb0d500aca51504de87a8837378561d
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2023-12-29 23:50:34 +0530

    Create 287-findtheduplicatenumber.java

---
+++ 287-findtheduplicatenumber.java
@@ -0,0 +1,12 @@
+// Takes 1ms
+class Solution {
+    public int findDuplicate(int[] nums) {
+        boolean[] arr = new boolean[nums.length];
+        for(int i:nums){
+            if(arr[i]) 
+                return i;
+            arr[i] = true;
+        }
+        return -1;
+    }
+}
