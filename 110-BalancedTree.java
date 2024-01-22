2ed6c4b9835e4ba7f09e0c91bc17c6786256e915
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2024-01-22 22:33:27 +0530

    Update 110-BalancedTree.java

--- 110-BalancedTree.java
+++ 110-BalancedTree.java
@@ -1,44 +1,32 @@
-// First Approach  Bottom DOWN
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int balance(TreeNode root){
-        if(root == null)
-            return 0;
-        return Math.max(balance(root.left),balance(root.right))+1;
-    }
     public boolean isBalanced(TreeNode root) {
-        if(root == null)
-            return true;
+        if(root == null)    return true;
 
-        int left = balance(root.left);
-        int right = balance(root.right);
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
 
-        if(Math.abs(left-right) <= 1 && isBalanced(root.left) && isBalanced(root.right)){
-            return true;
-        }
-        else 
-            return false;
+        if(Math.abs(leftHeight - rightHeight) >= 2) return false;
 
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    public int getHeight(TreeNode root) {
+        if(root == null)    return 0;
+
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
     }
 }
-
-
-// Second Approach Bottom UP 
-class Solution {
-    public int balance(TreeNode root){
-        if(root == null)
-            return 0;
-        int left = balance(root.left);
-        int right = balance(root.right);
-
-        if(left == -1 || right == -1)
-            return -1;
-
-        if(Math.abs(left- right) <= 1)
-            return Math.max(left,right)+1;
-        else 
-            return -1;
-    }
-    public boolean isBalanced(TreeNode root) {
-        return balance(root) != -1;
-    }
-} 
