bc8fea2802c2444365d6c31dc570a16d05e881e9
Akash Srivastava <113261623+aCash9@users.noreply.github.com>
2024-01-26 17:53:45 +0530

    Update 98-ValidateBinarySearchTree.java

--- 98-ValidateBinarySearchTree.java
+++ 98-ValidateBinarySearchTree.java
@@ -30,3 +30,18 @@ class Solution {
         return isValidBST(root.right);
     }
 }
+
+//another solution
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean helper(TreeNode root, long min, long max) {
+        if(root == null)    return true;
+
+        if(root.val <= min || root.val >= max)    return false;
+
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+}
